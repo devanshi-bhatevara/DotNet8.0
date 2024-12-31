910274e4ca63a6cdbb238f5bacd31fbae53efcd2
devanshi <devanshi.bhatevara@simformsolutions.com>
2024-12-31 15:28:45 +0530

    Complex types demo

---
+++ DotNet8.0/ComplexTypes/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8._0.ComplexTypes
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=EmployeeDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .OwnsOne(e => e.Address);
+        }
+    }
+}
