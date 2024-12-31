facb984e413edde302af63eb0984a12473ac997f
devanshi <devanshi.bhatevara@simformsolutions.com>
2024-12-31 14:36:31 +0530

    Some concepts: Alias any types, collection expressions, default lambda parameters, experimental attributes, primary ctor

---
+++ DotNet8.0/AliasAnyTypes.cs
@@ -0,0 +1,31 @@
+﻿//available previously for built-in types
+using MyConsole = System.Console;
+
+//now also available for tuples, pointers, arrays etc except nullables
+using Doctor = (int id, string name);
+
+namespace DotNet8._0
+{
+    public class AliasAnyTypes
+    {
+        public static void AliasAnyTypesDemo()
+        {
+            //earlier
+            MyConsole.WriteLine("Test console");
+
+            //new
+            Doctor doctor1 = new (1,"doctor1");
+
+            MyConsole.WriteLine("Details: "+ doctor1);
+
+        }
+
+        //can be passed as a parameter in methods too
+        public static void AliasAnyTypesDemo2(Doctor doctor)
+        {
+            MyConsole.WriteLine("Details: " + doctor);
+
+        }
+
+    }
+}
---
+++ DotNet8.0/CollectionExpressions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DotNet8._0
+{
+    public class CollectionExpressions
+    {
+        public static void CollectionExpressionsDemo() 
+        {
+           int[] myArray = [ 1, 2, 3 ];
+           List<int> myList = [1, 2, 3];
+           Span<int> mySpan = [1, 2, 3];
+
+
+            Console.WriteLine($"{nameof(myArray)}: {JsonSerializer.Serialize(myArray)}");
+            Console.WriteLine($"{nameof(myList)}: {JsonSerializer.Serialize(myList)}");
+            Console.Write($"{nameof(mySpan)}: ");
+            foreach (var item in mySpan) Console.Write($"{item} ");
+            Console.WriteLine();
+        }
+    }
+}
