f56a4de585eecb6db9dca724cb9fc6e17e022a20
devanshi <devanshi.bhatevara@simformsolutions.com>
2024-12-31 14:59:03 +0530

    Data annotations, frozen set and dictionary

---
+++ DotNet8.0/DataAnnotations.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet8._0
+{
+    public class DataAnnotations
+    {
+        [Length(2, 20)]
+        public string Username { get; set; }
+
+        [Range(1, 1000, MinimumIsExclusive = true, MaximumIsExclusive = false)]
+        public double PriceRange { get; set; }
+
+        [AllowedValues("Computers", "Video Games")]
+        public string Type { get; set; }
+
+        [DeniedValues("Printers")]
+        public string SubType { get; set; }
+    }
+}
